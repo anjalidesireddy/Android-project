99d0f1084716b96627e063933cb459522815d68f
Anjali <anjalidesireddy@gmail.com>
2018-01-03 12:05:19 +0530

    new features added
    new features added

---
+++ FragmentEx26/app/src/main/java/app/grepthor/fragmentex26/FragmentGalary.java
@@ -0,0 +1,43 @@
+package app.grepthor.fragmentex26;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentGalary extends Fragment {
+
+
+    public FragmentGalary() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_galary, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+}
